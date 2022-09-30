654df8a3405f15bf9f0a5e9f6d467d1e68bbeb72
natty <natnaelmasresha@gmail.com>
2022-09-30 23:37:06 +0300

    add functionality in trainer page

--- react/src/pages/Trainer.js
+++ react/src/pages/Trainer.js
@@ -1,28 +1,39 @@
 /*global reach*/
 import React, { useState } from 'react'
 import MyAlgoConnect from '@randlabs/myalgo-connect';
-import {createAsset,upload} from '../utils/asset'
+import { createAsset, upload } from '../utils/asset'
 const axios = require('axios')
 const algosdk = require('algosdk');
 const myAlgoConnect = new MyAlgoConnect();
 
 
+const mint = async (e) => {
+    
+    e.preventDefault()
 
-const mint = async () => {
-    let hash= await axios.post('http://127.0.0.1:8000/mint',{})
-    let assetID=await createAsset(hash)
+    let hash = await axios.post('http://127.0.0.1:8000/mint', {})
+    let assetID = await createAsset(hash)
+    let email = document.getElementById('email').value
+    console.log(email);
+    await axios.post('http://127.0.0.1:8000/mail', {"address":email,"asset_id":assetID})
+
     console.log(hash);
     console.log(assetID);
-    
+
 }
 
-const Trainer = () => {
-const [email, setemail] = useState("")
 
+const Trainer = () => {
+    
+    
+    const [trainees, setTrainees] = useState([])
+    
     return (
         <div>
-            <input type="text" name="" id="" placeholder='trainee@server.com' required />
-            <button onClick={mint}>Mint Asset</button>
+            <form onSubmit={mint}>
+                <input type="email" name="" id="email" required placeholder='trainee@server.com' />
+                <input type="submit" value="Mint Asset"/>
+            </form>
         </div>
     )
 }
